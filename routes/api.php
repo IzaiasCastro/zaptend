b5d179bb22fbe13c3d68633ae3345fe78068d563
Izaias Castro <30877119+IzaiasCastro@users.noreply.github.com>
2025-10-16 20:52:43 -0300

    Create /agendas endpoint for agenda datateste
    Add new endpoint to retrieve agendas with availability.

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/agendas', function (Request $request) {
+    $agendas = [
+        [
+            'barbeiro' => 'Carlos',
+            'horarios' => [
+                ['hora' => '09:00', 'disponivel' => true],
+                ['hora' => '10:00', 'disponivel' => false],
+                ['hora' => '11:00', 'disponivel' => true],
+            ],
+        ],
+        [
+            'barbeiro' => 'Marcos',
+            'horarios' => [
+                ['hora' => '09:00', 'disponivel' => true],
+                ['hora' => '10:00', 'disponivel' => true],
+                ['hora' => '11:00', 'disponivel' => false],
+            ],
+        ],
+    ];
+
+    return response()->json([
+        'success' => true,
+        'data' => $agendas,
+    ]);
+});
